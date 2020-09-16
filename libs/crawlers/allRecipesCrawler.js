10b7398aea85377696606050789d5fd807bd47f5
Kevin <jl.kevin.92@gmail.com>
2020-09-16 14:25:44 -0400

    ran lint

--- libs/crawlers/allRecipesCrawler.js
+++ libs/crawlers/allRecipesCrawler.js
@@ -1,46 +1,45 @@
 const request = require('request');
-const URL = require('url-parse');
-const path = require('path')
-const fs = require('fs')
+const path = require('path');
+const fs = require('fs');
 
-const baseUrl = "https://www.allrecipes.com/recipe/"
+const baseUrl = 'https://www.allrecipes.com/recipe/';
 
-const minIndex = 35118
-const maxIndex = 300000
-
-const startCrawling = async (callback) => {
-    for (let index = minIndex; index <= maxIndex; index++) {
-        const currentUrl = baseUrl + index
-        
-        request(currentUrl, function (error, response, body) {
-            if (error) {
-                console.error('error:', error); // Print the error if one occurred
-            }
-
-            console.log("currentIndex = "+ index)
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
-            if (response && response.statusCode == 200) {
-                const filePath = path.join(__dirname, "result", 'allRecipes' + index + ".txt");
-                console.log(filePath)
-                fs.writeFile(filePath, body, (err) => {
-                    if (err) throw err;
-                    console.log('The file has been saved!');
-                });
-            }
-
-        });
-        await sleep(2000)
-    }
-    return callback()
-};
+const minIndex = 35118;
+const maxIndex = 300000;
 
 function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+const startCrawling = async (callback) => {
+  for (let index = minIndex; index <= maxIndex; index += 1) {
+    const currentUrl = baseUrl + index;
+
+    request(currentUrl, (error, response, body) => {
+      if (error) {
+        console.error('error:', error); // Print the error if one occurred
+      }
+
+      console.log(`currentIndex = ${index}`);
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+
+      if (response && response.statusCode === 200) {
+        const filePath = path.join(__dirname, 'result', `allRecipes${index}.txt`);
+        console.log(filePath);
+        fs.writeFile(filePath, body, (err) => {
+          if (err) throw err;
+          console.log('The file has been saved!');
+        });
+      }
     });
-} 
+    // eslint-disable-next-line no-await-in-loop
+    await sleep(2000);
+  }
+  return callback();
+};
 
 module.exports = {
-    startCrawling
-}
+  startCrawling,
+};
